505c9d5052b5545a86258b799627b9e2c053eb3c
Dave Halliday <hallidave@gmail.com>
2021-11-17 10:31:17 -0500

    Merge multiple liveries for same airline

--- src/generate.py
+++ src/generate.py
@@ -8,6 +8,22 @@ aircraft_file = os.path.join(data_dir, "aircraft.json")
 liveries_file = os.path.join(data_dir, "liveries.json")
 output_file = os.path.join(src_dir, "..", "simple_traffic.vmr")
 
+def merge_airlines(model_liveries):
+    liveries_by_airline = {}
+    for livery in model_liveries:
+        icao_airline = livery["icao_airline"]
+        model_name = livery["title"]
+        if icao_airline in liveries_by_airline:
+            liveries_by_airline[icao_airline] = liveries_by_airline[icao_airline] + "//" + model_name
+        else:
+            liveries_by_airline[icao_airline] = model_name
+    
+    merged_liveries = []
+    for key, value in liveries_by_airline.items():
+        merged_liveries.append({"icao_airline": key, "title": value})
+    
+    return merged_liveries
+
 def generate_rules():
     with open(aircraft_file) as af, open(liveries_file) as lf:
         aircraft = json.load(af)
@@ -16,7 +32,7 @@ def generate_rules():
     for model, model_info in aircraft.items():
         for wtc_code, ac_types in model_info.items():
             for type_code in ac_types:
-                model_liveries = liveries[model][wtc_code]
+                model_liveries = merge_airlines(liveries[model][wtc_code])
                 for livery in model_liveries:
                         callsign_prefix = livery["icao_airline"]
                         model_name = livery["title"]
